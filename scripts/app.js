81889f3913742d8fada99f89738ac3af931830f1
aml131 <hopealasfoor@gmail.com>
2025-07-09 11:14:43 +0300

    create a grid

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,15 @@
 function init(){
-
+//all codes go here
+const gridElem = document.querySelector(".grid")
+function createGrid(){
+    //for every cell  create  DIV
+    //append this cell to our grid
+    for (let i=0; i<100; i++){
+        const cell = document.createElement('div')
+        cell.textContent= i
+        gridElem.appendChild(cell)
+    }
+}
+createGrid()
 }
 document.addEventListener('DOMContentLoaded',init)
