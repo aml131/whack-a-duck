68240c74297bef3694d366666c6e5406b89c6779
aml131 <hopealasfoor@gmail.com>
2025-07-09 13:33:13 +0300

    display duck

--- scripts/app.js
+++ scripts/app.js
@@ -3,12 +3,30 @@ function init() {
   const cells  = []
   const gridWidth = 10
   const numberOfCells = gridWidth * gridWidth
+
+  let duckPosition= 43
+
+function addDuck(){
+    cells[duckPosition].classList.add('duck')
+}
+function removeDuck(){
+    cells[duckPosition].classList.remove('duck')
+}
+function play(){
+    setInterval(()=>{
+        removeDuck()
+        duckPosition= Math.floor(Math.random()* numberOfCells)
+        addDuck()
+    },3000)
+}
+play()
+
   function createGrid() {
     // for every cell, create a div
     // then append the cell to the grid
     for (let i = 0; i < numberOfCells; i++) {
       const cell = document.createElement("div")
-      cell.classList.add('duck')
+      //cell.classList.add('duck')
       cell.textContent = i
       cells.push(cell)
       gridElem.appendChild(cell)
@@ -16,5 +34,7 @@ function init() {
     console.log(cells)
   }
   createGrid()
+addDuck() 
+removeDuck()
 }
 document.addEventListener("DOMContentLoaded", init)
